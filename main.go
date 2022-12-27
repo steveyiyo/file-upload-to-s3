91ae092c80e64506c4349b0691ece277c0b6a55a
steveyiyo <jackcooku@steveyi.net>
2022-12-28 00:16:14 +0800

    feat: 重新處理讀取 environment 流程

--- main.go
+++ main.go
@@ -6,30 +6,38 @@ import (
 
 	"github.com/joho/godotenv"
 	"github.com/steveyiyo/file-upload-to-s3/internal/s3api"
-	"github.com/steveyiyo/file-upload-to-s3/internal/webserver"
+	"github.com/steveyiyo/file-upload-to-s3/internal/web"
 )
 
-var S3_KeyID, S3_AppKey, S3_bucket, S3_Endpoint, S3_Region string
+var S3_KeyID, S3_AppKey, S3_bucket, S3_Endpoint, S3_Region, S3_URL_PATH string
 
 func init() {
+
 	// Get value from .env
 	err := godotenv.Load()
-	if err != nil {
-		log.Println("Error loading .env file")
-	}
 
-	S3_KeyID = os.Getenv("S3_KeyID")
-	S3_AppKey = os.Getenv("S3_AppKey")
-	S3_bucket = os.Getenv("S3_bucket")
-	S3_Endpoint = os.Getenv("S3_Endpoint")
-	S3_Region = os.Getenv("S3_Region")
+	S3_KeyID = os.Getenv("S3_KEYID")
+	S3_AppKey = os.Getenv("S3_APPKEY")
+	S3_bucket = os.Getenv("S3_BUCKET")
+	S3_Endpoint = os.Getenv("S3_ENDPOINT")
+	S3_Region = os.Getenv("S3_REGION")
+	S3_URL_PATH = os.Getenv("S3_URL_PATH")
 
-	if S3_KeyID == "" || S3_AppKey == "" || S3_bucket == "" || S3_Endpoint == "" || S3_Region == "" {
-		log.Fatal("Error to loading environment.")
+	if !checkEnvironment() {
+		if err != nil {
+			log.Fatal("Error loading .env file")
+		}
 	}
+}
+
+func checkEnvironment() bool {
+	if S3_KeyID == "" || S3_AppKey == "" || S3_bucket == "" || S3_Endpoint == "" || S3_Region == "" || S3_URL_PATH == "" {
+		return false
+	}
+	return true
 }
 
 func main() {
 	api1 := s3api.New(S3_KeyID, S3_AppKey, S3_bucket, S3_Endpoint, S3_Region)
-	webserver.Init(api1)
+	web.Init(api1)
 }
